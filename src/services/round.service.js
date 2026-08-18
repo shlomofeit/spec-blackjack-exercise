c6caf6320b26e9b6e82eae1d31c7397a100f752b
shlomofeit <11.5sfeit@gmail.com>
2026-08-18 18:46:58 +0300

    Added createRoundService app with all validation checks

--- src/services/round.service.js
+++ src/services/round.service.js
@@ -1,3 +1,6 @@
+import { errCreator } from "../utils.js";
+import { playerRepo } from "../repositories/player.repo.js";
+import { roundRepo } from "../repositories/round.repo.js";
 export function getRandomCard() {
   // prettier-ignore
   const rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
@@ -7,3 +10,39 @@ export function getRandomCard() {
   const randomSuit = suit[Math.floor(Math.random() * suit.length)];
   return { randomRank, randomSuit };
 }
+
+async function createRoundService(bet, player) {
+  if (!bet > 0 || !player.chips >= bet) {
+    throw errCreator(400, "Not enogh chips or incorrect bet");
+  }
+
+  const activeRound = await roundRepo.getActiveRoundByPlayerId(player.id);
+
+  if (!activeRound) {
+    throw errCreator(409, "There are already open round in progress");
+  }
+
+  const newRound = {
+    ...player,
+    playerCards: [getRandomCard(), getRandomCard()],
+    dealerCards: [getRandomCard(), getRandomCard()],
+    status: "in_prgress",
+    createdAt: new Date().toISOString(),
+  };
+  const roundId = await rounRepo.newRound(newRound);
+  const round = await rounRepo.getRoundById(roundId);
+  if (!round) {
+    throw errCreator();
+  }
+  const updatedPlayer = await playerRepo.chipsUpdate(player.id, bet);
+
+  const res = {
+    id: roundId,
+    playerCards: round.playerCards,
+    dealerUpCard: round.dealerCards[0],
+    chips: updatedPlayer.chips,
+  };
+  return res;
+}
+
+export const playerService = { createPlayerService };
