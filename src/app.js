c21a27f31eb637ff982880ab90997aeda8de05b2
shlomofeit <11.5sfeit@gmail.com>
2026-08-20 18:36:43 +0300

    Added app.use and installed cors by npm

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import playerRouter from "./routes/player.route.js";
 import roundRouter from "./routes/round.route.js";
 import dotenv from "dotenv";
-
+import cors from "cors";
 dotenv.config();
 
 function errorHandler(err, req, res, next) {
@@ -12,14 +12,18 @@ function errorHandler(err, req, res, next) {
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(process.cwd() + "/public"));
 
 app.use("/", (req, res, next) => {
   console.log(`${req.url} | ${req.method}`);
   next();
 });
 
-app.use("/start-game", playerRouter);
+app.use("/", playerRouter);
 app.use("/", roundRouter);
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
